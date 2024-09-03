a172446c1ea3ca0dc1734db7fc16b4a56424077d
Matutozi <sobowalegz2@gmail.com>
2024-09-03 17:00:43 +0100

    code that sets up the IR remote and output the a value corresponding to the value of button pressed

---
+++ IR_REMOTE/IR_remote.py
@@ -0,0 +1,18 @@
+from machine import Pin
+from ir_rx.nec import NEC_8
+from ir_rx.print_error import print_error
+irPin = 17
+
+myIR = Pin(irPin, Pin.IN)
+
+def callback(IRBit, x, y):
+    print(IRBit)
+    
+IR = NEC_8(myIR, callback)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    IR.close()
+    print("Program Terminated")
